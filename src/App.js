e1f175d8c91d13425d19bdbea7bdb3397deb2c57
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-10-22 08:48:17 +0900

    props

--- src/App.js
+++ src/App.js
@@ -12,17 +12,34 @@ import React, {Component} from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    {
+      name: "taro",
+      age: 10
+    },
+    {
+      name: "kenichi",
+      age: 43
+    },
+    {
+      name: "noname",
+    }
+  ]
   return(
     <div>
-      <Cat />{hoge}
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
-const hoge = "hogehoge"
-const Cat = () =>{
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, {props.age} years old! </div>
+}
+
+User.defaultProps = {
+  age : 1
 }
 export default App;
