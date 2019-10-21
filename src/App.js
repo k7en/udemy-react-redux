7cdc753fe4b700c7a5e27631fd3c38526837a0f9
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-10-22 08:36:09 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render(){
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am Clicked")}} />      
-      </React.Fragment>
-    )  
-  }
-}
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am Clicked")}} />      
+//       </React.Fragment>
+//     )  
+//   }
+// }
 
+const App = () => {
+  return(
+    <div>
+      <Cat />{hoge}
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+const hoge = "hogehoge"
+const Cat = () =>{
+  return <div>Meow!</div>
+}
 export default App;
