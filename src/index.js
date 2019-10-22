24f34bcd61f70986fca86a59e8648ab363ced106
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-10-22 18:56:07 +0900

    Mui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import './index.css';
 import reducer from './reducers'
@@ -13,11 +14,13 @@ import EventsNew from './components/events_new'
 import EventsShow from './components/events_show'
 import * as serviceWorker from './serviceWorker';
 
+
 const enhancer = process.env.NODE_ENV === 'development' ?
   composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore (reducer, enhancer)
 
 ReactDOM.render(
+  <MuiThemeProvider>
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
@@ -27,7 +30,8 @@ ReactDOM.render(
         <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
